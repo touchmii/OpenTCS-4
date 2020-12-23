f908ea7b3a5e3789e2b2cb2304eb23c26c4c5238
Benoni Jiang <ipeooc@gmail.com>
2020-12-23 10:43:04 +0800

    修复打包内核无法运行的问题，此问题是因为引用了opentcs-contrib-tcp-netty依赖。

--- openTCS-Example-CommAdapter-Jbh/src/main/java/com/lvsrobot/vehiclejbh/SocketUtils.java
+++ openTCS-Example-CommAdapter-Jbh/src/main/java/com/lvsrobot/vehiclejbh/SocketUtils.java
@@ -3,7 +3,7 @@ package com.lvsrobot.vehiclejbh;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
-import org.opentcs.contrib.tcp.netty.*;
+//import org.opentcs.contrib.tcp.netty.*;
 
 public class SocketUtils {
     private String ip;
